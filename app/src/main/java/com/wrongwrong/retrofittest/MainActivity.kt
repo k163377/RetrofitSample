a0ff2ee93a8767edebb7b90579359cfcc159512f
bntwng <boranti1995@gmail.com>
2018-08-12 00:53:46 +0900

    GithubAPIでリポジトリを取得し、0番目のfull_nameを表示する所まで

--- app/src/main/java/com/wrongwrong/retrofittest/MainActivity.kt
+++ app/src/main/java/com/wrongwrong/retrofittest/MainActivity.kt
@@ -2,11 +2,48 @@ package com.wrongwrong.retrofittest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gson = GsonBuilder().serializeNulls().create()
+        var retrofit: Retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .baseUrl("https://api.github.com/users/")
+                .build()
+        var service = retrofit.create(IGetRepos::class.java)
+        var call = service.GetRepos("k163377")
+        call.enqueue(object : Callback<List<Repo>> {
+            override fun onResponse(call: Call<List<Repo>>?, response: Response<List<Repo>>?) {
+                try{
+                    var arr: List<Repo>? = response!!.body()
+                    Log.d("onResponse", arr!![0].full_name)
+                }catch (e: IOException){
+                    Log.d("onResponse", "IOException")
+                }
+            }
+
+            override fun onFailure(call: Call<List<Repo>>?, t: Throwable?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
+    }
+
+    interface IGetRepos{
+        @GET("{id}/repos")
+        fun GetRepos(@Path("id") userID : String) : Call<List<Repo>>
     }
 }
